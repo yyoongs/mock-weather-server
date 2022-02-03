e1770e9ea37dbde9e7d09e117ea96be6129cf580
yyoongs <jysid3531@gmail.com>
2022-02-02 18:28:29 -0800

    Add check authHeader condition

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./modules/swagger.yaml');
 const bodyParser = require("body-parser");
-const axios = require('axios');
-const { json } = require('express');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,9 +40,11 @@ app.listen(port,host,() => {console.log('Node.js Express server is running on do
 })
 
 app.get('/v1/weather',(req,res) => {
-  var authHeaderToken = req.headers.authorization.replace('Bearer ', '');
-
-  if (authHeaderToken == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNob3lvbmdzIiwicGFzc3dvcmQiOiIxMjM0NTY3ODkifQ.XFjI5dtX3wbvK4Ps9q2F4A48sUw041oLQoiDYdOn5dg") {
+  var authHeaderToken = req.headers.authorization;
+  if (!authHeaderToken){
+    res.send("You need to request with authorization token")
+  }
+  else if (authHeaderToken.replace('Bearer ', '') == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNob3lvbmdzIiwicGFzc3dvcmQiOiIxMjM0NTY3ODkifQ.XFjI5dtX3wbvK4Ps9q2F4A48sUw041oLQoiDYdOn5dg") {
     const options = {
       uri: "https://api.openweathermap.org/data/2.5/weather",
       qs:{
@@ -61,16 +61,14 @@ app.get('/v1/weather',(req,res) => {
   else {
     res.send("Authorization failed.")
   }
-
-    
   });
 
 app.get('/v1/hello',(req,res) => {
-  // res.send("hello world! Welcome to CS561 assignment4 test API!\ndata : test")  
-  // get tocken by using v1/auth
-  var authHeaderToken = req.headers.authorization.replace('Bearer ', '');
-  console.log(authHeaderToken)
-  if (authHeaderToken == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNob3lvbmdzIiwicGFzc3dvcmQiOiIxMjM0NTY3ODkifQ.XFjI5dtX3wbvK4Ps9q2F4A48sUw041oLQoiDYdOn5dg") {
+  var authHeaderToken = req.headers.authorization;
+  if (!authHeaderToken) {
+    res.send("You need to request with authorization token")
+  }
+  else if (authHeaderToken.replace('Bearer ', '') == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNob3lvbmdzIiwicGFzc3dvcmQiOiIxMjM0NTY3ODkifQ.XFjI5dtX3wbvK4Ps9q2F4A48sUw041oLQoiDYdOn5dg") {
     res.send("hello world! Welcome to CS561 assignment4 test API!\nYou are authorized user!\n")
   }
   else {
